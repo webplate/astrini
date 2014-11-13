f377bbff90bb76af9e4a401038e68d56d869ad0c
Glen <glen@mailoo.org>
2014-11-13 01:36:21 +0100

    idea to use kwargs and completely define system in dictionnary

--- config.py
+++ config.py
@@ -95,3 +95,42 @@ ABOUTTEXT = [
 'Licence : GPL v3',
 'Contact : glenlomax@gmail.com',
 'The source code is available at : https://github.com/webplate/astrini']
+
+
+'''idea :
+a descrition of system should be enough??
+System = {'planetoids' :[
+        {'name' : 'sun',
+        'root' : self.root,
+        'tex' : 'sun_1k_tex.jpg',
+        'radius': SUNRADIUS_F,
+        'period' : SUNROT,
+        'offset' : 0,
+        'root_system',
+        'orbit_period',
+        'orbit_offset',
+        'orbit_radius'
+        }],
+    'orbitals' : [
+        {'name' : 'earth',
+        'root' : self.root,
+        'tex' : 'earth_1k_tex.jpg',
+        'radius': EARTHRADIUS_F,
+        'period' : 1,
+        'offset' : EARTHROTSET,
+        'root_system' : self.root,
+        'orbit_period' : EARTHREVO,
+        'orbit_offset' : EPHEMSIMPLESET,
+        'orbit_radius' : UA_F
+        },
+        {'name' : 'moon',
+        'root' : self.root,
+        'tex' : 'moon_1k_tex.jpg',
+        'radius': MOONRADIUS_F,
+        'period' : MOONROT,
+        'offset' : MOONROTSET,
+        'root_system' : self.earth.system,
+        'orbit_period' : MOONREVO,
+        'orbit_offset' : MOONEPHEMSET,
+        'orbit_radius' : MOONAX_F
+    }]'''
